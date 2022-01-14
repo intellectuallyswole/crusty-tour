aad636e16f92aa44f7757c424a828e46945d798b
Madelaine Boyd <madelaine.boyd+github@gmail.com>
2022-01-14 15:26:49 -0800

    Castro image appears at fixed location

--- CrustyTour/CrustyTour/CTARCLView.swift
+++ CrustyTour/CrustyTour/CTARCLView.swift
@@ -2,96 +2,32 @@
 //  CTARCLView.swift
 //  CrustyTour
 //
-//  Created by Madelaine Boyd on 1/13/22.
+//  Created by Madelaine Boyd on 1/14/22.
 //
 
 import Foundation
-
-import Foundation
-import RealityKit
+import SwiftUI
+import UIKit
 import ARKit
-import ARKit_CoreLocation
 
-class CTARCLView: UIViewController, ARSCNViewDelegate {
-    var arView: ARSCNView {
-       return self.view as! ARSCNView
+
+// Maybe use this to host in SwiftUI in the future
+// Overkill for now
+class CTARCLView : UIView {
+    var body: some View {
+      return CTARCLViewContainer().edgesIgnoringSafeArea(.all)
     }
-    var sceneLocationView: SceneLocationView {
-        return SceneLocationView()
+}
+
+struct CTARCLViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> CTARCLView {
+        let arView = CTARCLView()
+        return arView
+        
     }
     
-    override func viewDidLoad() {
-       super.viewDidLoad()
-        sceneLocationView.run()
-        self.view.addSubview(sceneLocationView)
-        
-        let material = SCNMaterial()
-        material.transparency = 0.5
-        material.diffuse.contents = UIColor.magenta
-        material.ambient.contents = UIColor.green
-        material.lightingModel = .constant
-        let torus = SCNTorus(ringRadius: 0.4, pipeRadius: 0.1)
-        torus.firstMaterial = material
-        let torusNode = SCNNode(geometry: torus)
-        torusNode.position = SCNVector3(0, 0, -0.2) // SceneKit/AR coordinates are in meters
-        arView.scene.rootNode.addChildNode(torusNode)
-       
-        let location = CLLocationCoordinate2DMake(39.23965369024172, -120.06924041182108)
-        let geoAnchor = ARGeoAnchor(coordinate: location)
-        arView.session.add(anchor: geoAnchor)
-    }
+    func updateUIView(_ uiView: CTARCLView, context: Context) {}
     
-
-    // MARK: - Functions for standard AR view handling
-    override func viewDidAppear(_ animated: Bool) {
-       super.viewDidAppear(animated)
-    }
-    override func viewDidLayoutSubviews() {
-       super.viewDidLayoutSubviews()
-        self.sceneLocationView.frame = view.bounds
-    }
-    override func viewWillAppear(_ animated: Bool) {
-       super.viewWillAppear(animated)
-       let configuration = ARWorldTrackingConfiguration()
-        
-        configuration.worldAlignment = .gravityAndHeading
-        
-        arView.session.run(configuration, options: [.resetTracking])
-       arView.delegate = self
-    }
-    override func viewWillDisappear(_ animated: Bool) {
-       super.viewWillDisappear(animated)
-       arView.session.pause()
-    }
-    // MARK: - ARSCNViewDelegate
-    func sessionWasInterrupted(_ session: ARSession) {}
-
-    func sessionInterruptionEnded(_ session: ARSession) {}
-    func session(_ session: ARSession, didFailWithError error: Error)
-    {}
-    func session(_ session: ARSession, cameraDidChangeTrackingState
-    camera: ARCamera) {}
     
-    func renderer(_ renderer: SCNSceneRenderer,
-                 didAdd node: SCNNode,
-                  for anchor: ARAnchor) {
-            
-        guard let geoAnchor = anchor as? ARGeoAnchor,
-                  geoAnchor.name == "Geo Anchor"
-        else { return }
-        
-        print(geoAnchor.coordinate)
-                
-        let boxGeometry = SCNBox(width: 1.0,
-                                height: 1.0,
-                                length: 1.0,
-                         chamferRadius: 0.1)
-
-        boxGeometry.firstMaterial?.diffuse.contents = UIColor.red
-
-        let cube = SCNNode(geometry: boxGeometry)
-        
-        node.addChildNode(cube)
-    }
-
 }
---
+++ CrustyTour/CrustyTour/CTARCLViewController.swift
@@ -0,0 +1,100 @@
+//
+//  CTARCLView.swift
+//  CrustyTour
+//
+//  Created by Madelaine Boyd on 1/13/22.
+//
+
+import Foundation
+
+import Foundation
+import RealityKit
+import ARKit
+import ARKit_CoreLocation
+
+class CTARCLViewController: UIViewController, ARSCNViewDelegate {
+    var sceneLocationView : SceneLocationView = SceneLocationView()
+    
+    func buildSceneLocationView() -> SceneLocationView {
+        let newSceneLocationView = SceneLocationView.init(trackingType: .worldTracking)
+        newSceneLocationView.arViewDelegate = self
+
+        newSceneLocationView.debugOptions = [.showWorldOrigin]
+        newSceneLocationView.showsStatistics = true
+        newSceneLocationView.showAxesNode = false // don't need ARCL's axesNode because we're showing SceneKit's
+        newSceneLocationView.autoenablesDefaultLighting = true
+        return newSceneLocationView
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sceneLocationView = self.buildSceneLocationView()
+        self.view.addSubview(sceneLocationView)
+        
+        let DODOWAH_BACKYARD = CLLocationCoordinate2DMake(39.23965369024172, -120.06924041182108)
+        //                                              +37.75574832,       -122.43995607
+        let GRAND_VIEW_AVE = CLLocationCoordinate2DMake(37.755776505037254, -122.4400525)
+        let LITERALLY_RIGHT_HERE = CLLocationCoordinate2DMake(37.75574832,       -122.43995607)
+        let location = CLLocation(coordinate: GRAND_VIEW_AVE, altitude: 122)
+        guard let image = UIImage.init(named: "Castro") else { return; }
+        let annotationNode = LocationAnnotationNode(location: location, image: image)
+        annotationNode.scaleRelativeToDistance = true
+        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: annotationNode)
+
+        
+//        let geoAnchor = ARGeoAnchor(coordinate: location)
+//        arView.session.add(anchor: geoAnchor)
+    }
+    
+    
+    // MARK: - Functions for standard AR view handling
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        sceneLocationView.frame = view.bounds
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.worldAlignment = .gravityAndHeading
+        sceneLocationView.run()
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneLocationView.pause()
+    }
+    // MARK: - ARSCNViewDelegate
+    func sessionWasInterrupted(_ session: ARSession) {}
+    
+    func sessionInterruptionEnded(_ session: ARSession) {}
+    func session(_ session: ARSession, didFailWithError error: Error)
+    {}
+    func session(_ session: ARSession, cameraDidChangeTrackingState
+                 camera: ARCamera) {}
+    
+    func renderer(_ renderer: SCNSceneRenderer,
+                  didAdd node: SCNNode,
+                  for anchor: ARAnchor) {
+
+        guard let geoAnchor = anchor as? ARGeoAnchor,
+              geoAnchor.name == "Geo Anchor"
+        else { return }
+
+        print(geoAnchor.coordinate)
+
+        let boxGeometry = SCNBox(width: 1.0,
+                                 height: 1.0,
+                                 length: 1.0,
+                                 chamferRadius: 0.1)
+
+        boxGeometry.firstMaterial?.diffuse.contents = UIColor.red
+
+        let cube = SCNNode(geometry: boxGeometry)
+
+        node.addChildNode(cube)
+    }
+    
+}
