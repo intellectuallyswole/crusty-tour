595184dfb1f252c9cf4f6323ade9346085d2eac4
Madelaine Boyd <madelaine.boyd+github@gmail.com>
2022-01-25 14:27:27 -0800

    Trying to texture the mesh without much success

--- CrustyTour-iOS/CrustyTour/CTARCLViewController.swift
+++ CrustyTour-iOS/CrustyTour/CTARCLViewController.swift
@@ -44,11 +44,6 @@ class CTARCLViewController: UIViewController, ARSCNViewDelegate {
         guard let mesh_obj = asset.object(at:0) as? MDLMesh else {
             fatalError("Failed to get mesh from asset.")
         }
-        let transform = MDLTransform.init()
-        transform.rotation = vector_float3(x:-.pi/2, y:0, z:0)
-        transform.scale = vector_float3(x:0.1,y:0.1,z:0.1)
-        transform.translation = vector_float3(x: 10, y: -20, z: -30)
-        mesh_obj.transform = transform
         // (lldb) po ((object as! MDLMesh).submeshes![0] as! MDLSubmesh).material!
         // <MDLMaterial: 0x2812c52c0>
         var node = SCNNode(mdlObject: mesh_obj)
@@ -68,13 +63,24 @@ class CTARCLViewController: UIViewController, ARSCNViewDelegate {
 //        }
         
         let material = SCNMaterial()
-        material.transparency = 0.5
-        material.diffuse.contents = UIImage.init(named:"N39W121-slope-012422-slope-colormap.png")
-        material.ambient.contents = UIColor.green
+        material.transparency = 1.0
+        let texture_img = UIImage.init(named:"N39W121-slope-012422-slope-colormap.png")
+        material.diffuse.contents = texture_img
+
+//        material.emission.contents = texture_img
+        material.diffuse.wrapS = .repeat
+        material.diffuse.wrapT = .repeat
+        material.isDoubleSided = true
+        // ???
+        material.ambient.contents = UIColor.black
         material.lightingModel = .constant
+        
         mesh_geometry.firstMaterial = material
         // Not sure if this line is necessary
         node = SCNNode(geometry: mesh_geometry)
+        node.rotation =  SCNVector4(x:1, y:0, z:0, w:-.pi/2)
+        node.scale =  SCNVector3(x:0.1,y:0.1,z:0.1)
+        node.position = SCNVector3(x: 10, y: -20, z: -30)
         // Transform the node to a lat/long location
         let locationNode = LocationNode(location:location)
         locationNode.addChildNode(node)
--- CrustyTour-iOS/CrustyTour/CTPlayground.playground/Contents.swift
+++ CrustyTour-iOS/CrustyTour/CTPlayground.playground/Contents.swift
@@ -1,3 +1,9 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+print("hello")
+
+let image = UIImage.init(named:"IMG_4480")
+print("hello")
+
