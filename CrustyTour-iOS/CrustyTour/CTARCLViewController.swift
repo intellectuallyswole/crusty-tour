53295f205727758edc3b19d407eba9642c963290
Madelaine Boyd <madelaine.boyd+github@gmail.com>
2022-02-02 21:40:46 -0800

    Attempt to translate mesh; fails.

--- CrustyTour-iOS/CrustyTour/CTARCLViewController.swift
+++ CrustyTour-iOS/CrustyTour/CTARCLViewController.swift
@@ -5,8 +5,7 @@
 //  Created by Madelaine Boyd on 1/13/22.
 //
 
-import Foundation
-
+import CoreLocation
 import Foundation
 import RealityKit
 import ARKit
@@ -15,7 +14,7 @@ import ModelIO
 import SceneKit
 import SceneKit.ModelIO
 
-class CTARCLViewController: UIViewController, ARSCNViewDelegate {
+class CTARCLViewController: UIViewController, ARSCNViewDelegate, CLLocationManagerDelegate {
     var sceneLocationView : SceneLocationView = SceneLocationView()
     
     func buildSceneLocationView() -> SceneLocationView {
@@ -35,6 +34,17 @@ class CTARCLViewController: UIViewController, ARSCNViewDelegate {
         self.view.addSubview(sceneLocationView)
         
         let location = N39W121_ORIGIN_LOCATION
+//        let location = DODOWAH_BACKYARD_LOCATION
+        
+        let locManager = CLLocationManager()
+        locManager.delegate = self
+        locManager.desiredAccuracy = kCLLocationAccuracyBest
+        locManager.startUpdatingLocation()
+        // Wait in delegate method for an actual location?
+        guard let currentLocation = locManager.location else {
+            fatalError("Could not get current location")
+        }
+        let translation = currentLocation.translation(toLocation: location)
         let bundle = Bundle.main
         let path = bundle.path(forResource:"CrustyTour.scnassets/N39W121_translate_hmm_obj", ofType: "obj")
         let url = URL(fileURLWithPath: path!)
@@ -44,11 +54,17 @@ class CTARCLViewController: UIViewController, ARSCNViewDelegate {
         }
         let elevationMesh = SCNGeometry(mdlMesh: mesh_obj)
         let node = SCNNode(geometry: elevationMesh)
-        let locationNode = LocationNode(location:location)
-        locationNode.addChildNode(node)
+//        node.localTranslate(by: SCNVector3(x:Float(translation.longitudeTranslation), y:Float(translation.altitudeTranslation), z: Float(-1.0*translation.latitudeTranslation)))
+//        node.pivot = SCNMatrix4MakeTranslation(Float(-1.0*translation.longitudeTranslation), Float(-1.0*translation.altitudeTranslation), Float(translation.latitudeTranslation))
+//        let locationNode = LocationNode(location:location)
+//        locationNode.addChildNode(node)
+        
+        // change camera.zFar?
         
         // and add it to the AR scene.
-        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: locationNode)
+//        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: locationNode)
+//        sceneLocationView.scene.rootNode.camera?.zFar = 120000
+        sceneLocationView.scene.rootNode.addChildNode(node)
     }
     
     
